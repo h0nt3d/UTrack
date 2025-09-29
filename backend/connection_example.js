165633cca296ffafddd6b2d909433a1891c37eac
h0nt3d <akanoim@gmail.com>
2025-09-29 18:22:09 -0300

    Added Example for connection to MongoDB and storing data

---
+++ backend/connection_example.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+//Connecting to MongoDB
+mongoose.connect("mongodb://admin:password@localhost:27017/school?authSource=admin", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+//Schema Definition
+const instructorSchema = new mongoose.Schema ({
+	username: {type: String, required: true, unique: true},
+	password: {type: String, required: true}
+});
+
+const Instructor = mongoose.model("Instructor", instructorSchema);
+
+//Creating User
+const saltRounds = 10;
+async function createUser(username, password) {
+	try {
+		const hash = await bcrypt.hash(password, saltRounds);
+
+		const newInstructor = new Instructor({
+			username,
+			password: hash,
+		});
+		
+		//save to MongoDB
+		await newInstructor.save();
+		console.log("User saved: ", newInstructor);
+	}
+	catch(err) {
+		console.error("Error saving user: ", err);
+	}
+	finally {
+		mongoose.connection.close();
+	}
+}
+
+createUser("Michael", "super-secret-password");
