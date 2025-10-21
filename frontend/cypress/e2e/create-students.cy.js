2457c6a44791e1625e4ba4220bf4210ff91ce5d6
bmacgill87 <benedict.macgillivray@unb.ca>
2025-10-21 16:41:45 -0300

    Create Students Acceptance Test Skeleton

---
+++ frontend/cypress/e2e/create-students.cy.js
@@ -0,0 +1,68 @@
+/**
+ * This file contains all cypress acceptance tests related to the Create Students epic.
+ */
+
+describe("Create Students", () => {
+
+    // Roster Button
+    it("Displays student roster when Course Roster is clicked.", () => {
+        //
+    });
+
+    // Empty Roster
+    it("Displays an empty roster message if there are no students in the roster.", () => {
+        //
+    });
+
+    // Roster Error
+    it("Displays an error if the roster is not successfully retrieved.", () => {
+        //
+    });
+
+    // Add Student
+    it("Displays the add student page when clicked.", () => {
+        //
+    });
+
+    // Manual Upload
+    it("Adds a single student with valid input.", () => {
+        //
+    });
+
+    // Registers Student
+    it("Adds a student to a course when the student account already exists.", () => {
+        //
+    });
+
+    // Invalid Student Email Validation
+    it("Displays an error when adding a student with an invalid email.", () => {
+        //
+    });
+
+    // Empty Fields Validation
+    it("Displays an error when adding a student without all required fields.", () => {
+        //
+    });
+
+    // Duplicate Students
+    it("Displays an error when a student is already enrolled in a course.", () => {
+        //
+    });
+
+    // Bulk Upload
+    it("Adds a group of students given a csv or xlsx file.", () => {
+        //
+    });
+
+    // Empty File
+    it("Displays an error when giving an empty csv or xlsx file to the bulk upload.", () => {
+        //
+    });
+
+    // Invalid File Type
+    it("Displays an error when giving a non csv or xlsx file to the bulk upload.", () => {
+        //
+    });
+
+
+});
