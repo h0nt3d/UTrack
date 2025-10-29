232a5349042ecf24be11b21313ee594fbc35bcdb
Michael <120585383+h0nt3d@users.noreply.github.com>
2025-10-29 19:49:09 -0300

    Refactor INSTALL_DEPENDENCIES.bat

--- INSTALL_DEPENDENCIES.bat
+++ INSTALL_DEPENDENCIES.bat
@@ -1,12 +1,9 @@
-:: Start installing backend dependencies in a new terminal
 start cmd /k "cd backend && npm install prompt-sync bcrypt mongoose cors express dotenv"
 
-:: Wait for 2 seconds
 timeout /t 2
 
-:: Start installing frontend dependencies in a new terminal
 start cmd /k "cd frontend && npm install react-scripts react-router-dom lucide-react xlsx"
 
-:: Finish
 echo Done starting backend and frontend installations
 pause
+
