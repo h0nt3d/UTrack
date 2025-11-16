d6c5d0361fc1bac2e9ce14f23150172943a4f882
Michael <120585383+h0nt3d@users.noreply.github.com>
2025-11-16 11:28:12 -0400

    Fix installation commands for frontend dependencies

--- INSTALL_DEPENDENCIES.bat
+++ INSTALL_DEPENDENCIES.bat
@@ -2,8 +2,9 @@ start cmd /k "cd backend && npm install prompt-sync bcrypt mongoose cors express
 
 timeout /t 2
 
-start cmd /k "cd frontend && npm install react-scripts react-router-dom lucide-react xlsx"
+start cmd /k "cd frontend && npm install react-scripts react-router-dom lucide-react xlsx date-fns"
 
 echo Done starting backend and frontend installations
 pause
+
 
