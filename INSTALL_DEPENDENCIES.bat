f560f20f72b43a3192a7bcb9ccfcb9f014dd3b52
h0nt3d <akanoim@gmail.com>
2025-12-03 15:14:53 -0400

    Modified bat INSTALL script

--- INSTALL_DEPENDENCIES.bat
+++ INSTALL_DEPENDENCIES.bat
@@ -1,10 +1,10 @@
-start cmd /k "cd backend && npm install prompt-sync bcrypt mongoose cors express dotenv"
+start cmd /k "cd backend && npm install prompt-sync bcrypt mongoose cors express express-validator dotenv jsonwebtoken"
 
 timeout /t 2
 
 start cmd /k "cd frontend && npm install react-scripts react-router-dom lucide-react xlsx date-fns"
 
-echo Done starting backend and frontend installations
+echo Done installing dependencies
 pause
 
 
